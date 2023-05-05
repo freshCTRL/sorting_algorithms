1298d15facae0d94de13c31d35c118d32eb6c56d
freshCTRL <oyeboabariu25@gmail.com>
2023-05-05 10:08:16 -0700

    algorithms

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -5,16 +5,16 @@
  * @size: argument to bubble_sort
  *
  * Description - sorts the array by immediate pair of unordered set
-*/
+ */
 void bubble_sort(int *array, size_t size)
 {
 size_t i, j;
 char k;
 j = 0;
-while (j < size)
+while (j < size - 1)
 {
 i = 0;
-while (i <= size - 2)
+while (i < size - 1)
 {
 if (array[i] > array[i + 1])
 {
