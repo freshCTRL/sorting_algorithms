b5cd906aabf7fc0227edfdcfeac5e2b7f3613666
freshCTRL <oyeboabariu25@gmail.com>
2023-05-07 00:19:22 -0700

    algorithms

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,11 +1,103 @@
 #include "sort.h"
+#include "string.h"
+void swap_us(listint_t **head, listint_t *ptra, listint_t *ptrb);
+/**
+ * swap_us - entry_point.
+ * @head: head of the whole list passed
+ * @ptra: node to be swap with ptr2
+ * @ptrb: node to be swap with ptr1
+ * Description - this function swaps two nodes
+*/
+void swap_us(listint_t **head, listint_t *ptra, listint_t *ptrb)
+{
+listint_t *store, *store1, *p1, *p2;
+if ((ptra->prev != NULL) && (ptrb->next != NULL))
+{
+p1 = ptra->next;
+p2 = ptrb->prev;
+if (ptra->next == ptrb)
+{
+store = ptra->prev;
+store1 = ptrb->next;
+ptra->next = store1;
+ptra->prev = ptrb;
+ptrb->next = ptra;
+ptrb->prev = store;
+store->next = ptrb;
+store1->prev = ptra;
+}
+else
+{
+store = ptra->prev;
+store1 = ptrb->next;
+ptra->next = store1;
+ptra->prev = ptrb;
+ptrb->next = ptra;
+ptrb->prev = store;
+p2->next = ptra;
+p1->prev = ptrb;
+store->next = ptrb;
+store1->prev = ptra;
+}
+}
+if ((ptrb->next == NULL) && (ptra->prev != NULL))
+{
+store = ptra->prev;
+ptra->next = NULL;
+ptra->prev = ptrb;
+ptrb->next = ptra;
+ptrb->prev = store;
+store->next = ptrb;
+}
+if ((ptra->prev == NULL) && (ptrb->prev != NULL))
+{
+store = ptrb->next;
+ptra->next = store;
+ptra->prev = ptrb;
+ptrb->prev = NULL;
+ptrb->next = ptra;
+store->prev = ptra;
+*head = ptrb;
+}
+if ((ptra->prev == NULL) && (ptrb->next == NULL))
+{
+store = ptrb->prev;
+store1 = ptra->next;
+ptra->next = NULL;
+ptra->prev = store;
+ptrb->prev = NULL;
+ptrb->next = store1;
+store->next = ptra;
+store1->prev = ptrb;
+*head = ptrb;
+}
+}
 /**
  * insertion_sort_list - Entry point
- * @list: argument to main
- *
+ * @list: argument to insertion_sort_list
  * Description: sort a list by insertion method
 */
 void insertion_sort_list(listint_t **list)
 {
-
+listint_t *mst = NULL, *temp, *lst;
+int i, d, g;
+if ((*list == NULL) || ((*list)->next == NULL))
+return;
+lst = *list;
+for (i = 0; lst; i++)
+lst = lst->next;
+for (g = 1; g < i; g++)
+{
+mst = *list;
+for (d = 1; d < g; d++)
+mst = mst->next;
+temp = mst->next;
+for (d = g; ((d >= 1) && (mst->n > temp->n)); d--)
+{
+swap_us(list, mst, temp);
+if (temp->prev != NULL)
+mst = temp->prev;
+print_list(*list);
+}
+}
 }
