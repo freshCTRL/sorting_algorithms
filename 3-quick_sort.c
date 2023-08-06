8a8386aa2318c0b279a3d6da55b5c523f6c127b1
freshCTRL <111054651+freshCTRL@users.noreply.github.com>
2023-08-06 16:16:36 +0100

    sorting

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,51 +1,49 @@
 #include "sort.h"
 /**
- * lomuto - Entry point
- * @array: argument to lomuto
- * @size: argument to lomuto
- * @high: argument to lomuto
- * @low: argument to lomuto
- *
- * Description - a lomuto partitioning scheme use for quick_sort algorithm.
+* lomuto - Entry point
+* @array: argument to lomuto
+* @size: argument to lomuto
+* @high: argument to lomuto
+* @low: argument to lomuto
+*
+* Description - a lomuto partitioning scheme use for quick_sort algorithm.
 */
 void lomuto(int *array, int low, int high, size_t size);
 
-void lomuto(int *array, int low, int high, size_t size) {
-  int i, j, pivot, temp;
-  
-  if (low >= high)
-    return;
-  
-  i = low;
-  j = low;
-  pivot = array[high];
-  
+void lomuto(int *array, int low, int high, size_t size)
+{
+int i, j, pivot, temp;
 
-  for (i=low; i<=high; i++) {
-    if (array[i]<=pivot) {
-      temp=array[i];
-      array[i]=array[j];
-      array[j]=temp;
-      if (array[i] != array[j]) {
-        print_array(array, size);
-      }
-      j++;
-    }
-  }
-  
-  lomuto(array, low, j - 2, size);
-  lomuto(array, j, high, size);
+if (low >= high)
+return;
+i = low;
+j = low;
+pivot = array[high];
+for (i=low; i<=high; i++)
+{
+if (array[i]<=pivot)
+{
+temp=array[i];
+array[i]=array[j];
+array[j]=temp;
+if (array[i] != array[j])
+print_array(array, size);
+j++;
+}
+}
+lomuto(array, low, j - 2, size);
+lomuto(array, j, high, size);
 }
 /**
- * quick_sort - Entry point
- * @array: argument to quick_sort
- * @size: argument to quick_sort
- *
- * Description - a sorting algorithm that use lomuto partitioning scheme
+* quick_sort - Entry point
+* @array: argument to quick_sort
+* @size: argument to quick_sort
+*
+* Description - a sorting algorithm that use lomuto partitioning scheme
 */
-void quick_sort(int *array, size_t size) {
+void quick_sort(int *array, size_t size)
+{
 size_t high;
-
 if (size == 0)
 return;
 high = size - 1;
